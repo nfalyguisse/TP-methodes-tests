9182413d17e893021e269818281d9d35a358a28a
nfalyguisse <nfalyguisse72@gmail.com>
2025-04-10 10:21:36 +0200

    fsdf

--- src/main/java/ng/ynov/tp2articlesmanager/service/ArticleService.java
+++ src/main/java/ng/ynov/tp2articlesmanager/service/ArticleService.java
@@ -1,12 +1,14 @@
 package ng.ynov.tp2articlesmanager.service;
 
+import java.util.List;
+
 import ng.ynov.tp2articlesmanager.model.Article;
 
 public abstract class ArticleService {
 
     public abstract Article addArticle(String nom, double prix);
 
-    public abstract Article deleteArticle(long id);
+    public abstract boolean deleteArticle(long id);
 
-    public abstract boolean getAllArticles();
+    public abstract List<Article> getAllArticles();
 }
